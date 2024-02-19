d28117cb21020901680f6b08f0f19eee632ce4ea
Samuel Ayer <ayersamuel07@gmail.com>
2024-02-19 05:59:45 +0000

    Add variables

---
+++ Module01-Docker-Postgres-Terraform/Terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  description = "My credentials"
+  default = "~/.gc/terra-creds.json"
+}
+
+variable "project" {
+    description = "Project"
+    default = "nimble-volt-413114"
+}
+
+variable "region" {
+  description = "Project region"
+  default = "us-west4"
+}
+
+variable "location" {
+  description = "Project Location"
+  default = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery"
+  default = "terra_demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket"
+  default = "nimble-volt-413114-terra-bucket"
+}
+
+variable "gsc_storage_class" {
+  description = "Bucket Storage Class"
+  default = "STANDARD"
+}
